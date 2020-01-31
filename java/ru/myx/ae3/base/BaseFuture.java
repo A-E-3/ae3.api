3a5d080dded237501796e626d7ee102ad7401ae4
Alexander Kharichev <myx@myx.co.nz>
2020-01-31 14:38:25 +0200

    * source formatting

--- java/ru/myx/ae3/base/BaseFuture.java
+++ java/ru/myx/ae3/base/BaseFuture.java
@@ -6,131 +6,80 @@ import ru.myx.ae3.reflect.ReflectionExplicit;
 import ru.myx.ae3.reflect.ReflectionHidden;
 import ru.myx.ae3.reflect.ReflectionManual;
 
-/**
+/** @author myx
  *
- * @author myx
- *
- * @param <V>
- */
+ * @param <V> */
 @ReflectionManual
 public interface BaseFuture<V> extends BaseObject, FutureValue<V> {
 	
-	
-	/**
-	 *
-	 * @author myx
-	 * @param <V>
-	 *
-	 */
+	/** @author myx
+	 * @param <V> */
 	@ReflectionManual
 	public static interface Completable<V> extends BaseFuture<V> {
-		
 		
 		/**
 		 *
 		 */
 		static BaseObject PROTOTYPE = Reflect.classToBasePrototype(BaseFuture.Completable.class);
-		
-		/**
-		 *
-		 * @param error
-		 */
-		@ReflectionExplicit
-		void setError(Object error);
-		
-		/**
-		 *
-		 * @param result
-		 */
-		@ReflectionExplicit
-		void setResult(V result);
-		
-		/**
-		 *
-		 * @return BaseFuture.PROTOTYPE by default
-		 */
+
+		/** @return BaseFuture.PROTOTYPE by default */
 		@Override
 		default BaseObject baseFutureType() {
 			
-			
 			return BaseFuture.Completable.PROTOTYPE;
 		}
+
+		/** @param error */
+		@ReflectionExplicit
+		void setError(Object error);
+
+		/** @param result */
+		@ReflectionExplicit
+		void setResult(V result);
 	}
-	
-	/**
-	 * Default base future prototype
-	 */
+
+	/** Default base future prototype */
 	@ReflectionHidden
 	static BaseObject PROTOTYPE = Reflect.classToBasePrototype(BaseFuture.class);
-	
-	/**
-	 *
-	 * @return BaseFuture.PROTOTYPE by default
-	 */
+
+	/** @return BaseFuture.PROTOTYPE by default */
 	@ReflectionHidden
 	default BaseObject baseFutureType() {
 		
-		
 		return BaseFuture.PROTOTYPE;
 	}
-	
-	/**
-	 * return Base.forUnknown(this.baseValue());
-	 */
-	@Override
-	@ReflectionHidden
-	default BaseObject basePrototype() {
-		
-		
-		return Base.forUnknown(this.baseValue());
-	}
-	
-	/**
-	 * @return null
-	 */
+
+	/** @return null */
 	@Override
 	@ReflectionHidden
 	default BaseProperty baseGetOwnProperty(final BasePrimitiveString name) {
 		
-		
 		// System.out.println(">>> >>> GOPB: " + this.isDone() + ", name: " +
 		// name);
 		return this.baseFutureType().baseFindProperty(name);
 	}
-	
-	/**
-	 * @return null
-	 */
+
+	/** @return null */
 	@Override
 	@ReflectionHidden
 	default BaseProperty baseGetOwnProperty(final String name) {
 		
-		
 		// System.out.println(">>> >>> GOPB: " + this.isDone() + ", name: " +
 		// name);
 		return this.baseFutureType().baseFindProperty(name);
 	}
-	
-	/**
-	 * Must return future value, wait until complete
-	 */
-	@Override
-	V baseValue();
-	
+
+	/** return Base.forUnknown(this.baseValue()); */
 	@Override
 	@ReflectionHidden
-	default BasePrimitiveString baseToString() {
+	default BaseObject basePrototype() {
 		
-		
-		final V value = this.baseValue();
-		assert value != this : "Future base value is THIS, class: " + this.getClass().getName();
-		return Base.forUnknown(this.baseValue()).baseToString();
+		return Base.forUnknown(this.baseValue());
 	}
-	
+
 	@Override
 	@ReflectionHidden
 	default BasePrimitiveBoolean baseToBoolean() {
-		
 		
 		if (!this.baseAwait()) {
 			return BaseObject.FALSE;
@@ -155,4 +104,17 @@ public interface BaseFuture<V> extends BaseObject, FutureValue<V> {
 		}
 		return BaseObject.TRUE;
 	}
+
+	@Override
+	@ReflectionHidden
+	default BasePrimitiveString baseToString() {
+		
+		final V value = this.baseValue();
+		assert value != this : "Future base value is THIS, class: " + this.getClass().getName();
+		return Base.forUnknown(value).baseToString();
+	}
+
+	/** Must return future value, wait until complete */
+	@Override
+	V baseValue();
 }
