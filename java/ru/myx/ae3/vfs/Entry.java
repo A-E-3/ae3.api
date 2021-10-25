078ceae63f7d678d5f65050a7bb6de4caf8e3a92
Alexander Kharichev <myx@myx.co.nz>
2021-10-25 19:51:42 +0300

    * minor bulk changes

--- java/ru/myx/ae3/vfs/Entry.java
+++ java/ru/myx/ae3/vfs/Entry.java
@@ -19,13 +19,97 @@ import ru.myx.ae3.reflect.ReflectionManual;
 /** @author myx */
 @ReflectionManual
 public interface Entry extends EntryChange /* FIXME: temporarily, decouple later */ {
-	
+
 	/**
 	 *
 	 */
 	@ReflectionExplicit
 	static BaseObject PROTOTYPE = Reflect.classToBasePrototype(Entry.class);
-	
+
+	/** - empty path returns not 'this' but 'null';
+	 *
+	 * - paths starting with '/' return 'null' if share is null
+	 *
+	 * @param share
+	 * @param focus
+	 * @param path
+	 * @param mode
+	 * @return */
+	@ReflectionHidden
+	static Entry relative(final Entry share, final Entry focus, final String path, final TreeLinkType mode) {
+
+		if (path == null) {
+			throw new NullPointerException("'path' is null!");
+		}
+		if (path.length() == 0) {
+			return null;
+		}
+		Entry current;
+
+		if (path.charAt(0) == '/') {
+			if (share == null) {
+				return null;
+			}
+			current = share;
+		} else {
+			current = focus;
+		}
+
+		if (!current.isContainer()) {
+			if (mode == null) {
+				return null;
+			}
+			throw new IllegalArgumentException(
+					(current.isExist()
+						? "Not a container: "
+						: "Doesn't exist: ") + current.getLocation());
+		}
+		for (final StringTokenizer tokenizer = new StringTokenizer(path, "/"); tokenizer.hasMoreTokens();) {
+			final String part = tokenizer.nextToken();
+			final int length = part.length();
+			if (length == 0) {
+				continue;
+			}
+			if (length == 1 && '.' == part.charAt(0)) {
+				continue;
+			}
+			if (length == 2 && '.' == part.charAt(0) && '.' == part.charAt(1)) { // "..".equals(part)
+				if (current == share) {
+					if (mode == null) {
+						return null;
+					}
+					throw new IllegalArgumentException("Cannot go upper than share!");
+				}
+				current = current.getParent();
+				assert current != null : "NULL parent is unexpected!";
+				if (!current.isExist()) {
+					if (mode == null) {
+						return null;
+					}
+					throw new IllegalArgumentException("doesn't exist: " + current.getLocation());
+				}
+			} else {
+				if (mode != null && !current.isContainer() && current.isExist()) {
+					throw new IllegalArgumentException("Not a container: " + current.getLocation());
+				}
+				final Value<? extends Entry> element = current.toContainer().getContentElement(part, mode);
+				if (mode != null && element instanceof Entry && !tokenizer.hasMoreTokens()) {
+					return (Entry) element;
+				}
+				current = element.baseValue();
+				assert mode == null || current != null : "Should not create NULL entries when mode is not NULL";
+				if (current == null) {
+					return null;
+				}
+				if (mode == null && !current.isExist()) {
+					return null;
+				}
+			}
+		}
+		return current;
+
+	}
+
 	/** TODO replace with createChange(EntryChange parentChange);
 	 *
 	 * Returns ability to make changes in given node.
@@ -33,7 +117,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	boolean canWrite();
-	
+
 	/** Creates cloned entry with parent equal to passed one
 	 *
 	 * @param parent
@@ -43,7 +127,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	Entry clone(Entry parent, String key, TreeLinkType mode);
-	
+
 	/** moves/renames given entry.
 	 *
 	 * @param newEntry
@@ -51,7 +135,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return succeeds only if both entries are located in the same storage. */
 	@ReflectionExplicit
 	Value<Boolean> doMoveRename(Entry newEntry);
-	
+
 	/** renames given entry. Equivalent to:
 	 *
 	 * <pre>
@@ -63,7 +147,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	Value<Boolean> doRename(String newKey);
-	
+
 	/** Tries to return binary content for any possible type of entry.
 	 *
 	 * If entry is character - the UTF-8 character set is used.
@@ -111,35 +195,35 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 		}
 		throw new UnsupportedOperationException("Can't produce binary content, entryClass: " + this.getClass().getSimpleName());
 	}
-	
+
 	/** Returns entry key. Returns NULL when this entry is a root entry.
 	 *
 	 * @return key */
 	@ReflectionExplicit
 	String getKey();
-	
+
 	/** Returns 'last-modified' date of given link.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	long getLastModified();
-	
+
 	/** Returns location path in hierarchy. '' for root entry and so on separated by '/'.
 	 *
 	 * @return location */
 	@ReflectionExplicit
 	String getLocation();
-	
+
 	/** @return */
 	@ReflectionExplicit
 	TreeLinkType getMode();
-	
+
 	/** Returns parent entry of this entry. Returns NULL when this entry is a root entry.
 	 *
 	 * @return parent entry */
 	@ReflectionExplicit
 	Entry getParent();
-	
+
 	/** Works when isPrimitive() method returns TRUE.
 	 *
 	 * Value is always immediately available, so return type is Object.
@@ -147,7 +231,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionHidden
 	default Object getPrimitiveValue() {
-		
+
 		return null;
 	}
 
@@ -160,7 +244,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 
 		return BaseObject.UNDEFINED;
 	}
-	
+
 	/** Tries to return text content for any possible type of entry.
 	 *
 	 * If entry is binary - the UTF-8 character set is used.
@@ -169,7 +253,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @throws UnsupportedOperationException */
 	@ReflectionExplicit
 	default Value<? extends CharSequence> getTextContent() throws UnsupportedOperationException {
-		
+
 		if (this.isCharacter()) {
 			final EntryCharacter character = this.toCharacter();
 			if (character != null) {
@@ -236,16 +320,16 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 		}
 		throw new UnsupportedOperationException("Entry doesn't have meaningful value: " + this);
 	}
-	
+
 	/** Indicates whether this Entry is exist and has binary content.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isBinary() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry is exist and has text content. (not all storages support
 	 * explicit text content).
 	 *
@@ -255,156 +339,72 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default boolean isCharacter() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry is exist and is a collection.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isContainer() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry exists on persistent storage.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isExist() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry is exist and is a file (primitive or binary), not a collection.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isFile() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry is hidden.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isHidden() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry is a mount.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isMount() {
-		
+
 		return false;
 	}
-	
+
 	/** Indicates whether this Entry is exist and has primitive content.
 	 *
 	 * @return */
 	@ReflectionExplicit
 	default boolean isPrimitive() {
-		
+
 		return false;
 	}
-	
-	/** - empty path returns not 'this' but 'null';
-	 *
-	 * - paths starting with '/' return 'null' if share is null
-	 *
-	 * @param share
-	 *
-	 * @param path
-	 * @param mode
-	 * @return */
-	@ReflectionHidden
-	default Entry relative(final Entry share, final String path, final TreeLinkType mode) {
-		
-		if (path == null) {
-			throw new NullPointerException("'path' is null!");
-		}
-		if (path.length() == 0) {
-			return null;
-		}
-		Entry current;
-		
-		if (path.charAt(0) == '/') {
-			if (share == null) {
-				return null;
-			}
-			current = share;
-		} else {
-			current = this;
-		}
-		
-		if (!current.isContainer()) {
-			if (mode == null) {
-				return null;
-			}
-			throw new IllegalArgumentException(
-					(current.isExist()
-						? "Not a container: "
-						: "Doesn't exist: ") + current.getLocation());
-		}
-		for (final StringTokenizer tokenizer = new StringTokenizer(path, "/"); tokenizer.hasMoreTokens();) {
-			final String part = tokenizer.nextToken();
-			final int length = part.length();
-			if (length == 0) {
-				continue;
-			}
-			if (length == 1 && '.' == part.charAt(0)) {
-				continue;
-			}
-			if (length == 2 && '.' == part.charAt(0) && '.' == part.charAt(1)) { // "..".equals(part)
-				if (current == share) {
-					if (mode == null) {
-						return null;
-					}
-					throw new IllegalArgumentException("Cannot go upper than share!");
-				}
-				current = current.getParent();
-				assert current != null : "NULL parent is unexpected!";
-				if (!current.isExist()) {
-					if (mode == null) {
-						return null;
-					}
-					throw new IllegalArgumentException("doesn't exist: " + current.getLocation());
-				}
-			} else {
-				if (mode != null && !current.isContainer() && current.isExist()) {
-					throw new IllegalArgumentException("Not a container: " + current.getLocation());
-				}
-				final Value<? extends Entry> element = current.toContainer().getContentElement(part, mode);
-				if (mode != null && element instanceof Entry && !tokenizer.hasMoreTokens()) {
-					return (Entry) element;
-				}
-				current = element.baseValue();
-				assert mode == null || current != null : "Should not create NULL entries when mode is not NULL";
-				if (current == null) {
-					return null;
-				}
-				if (mode == null && !current.isExist()) {
-					return null;
-				}
-			}
-		}
-		return current;
-		
-	}
-	
+
 	/** @param path
 	 * @return */
 	@ReflectionExplicit
 	default Entry relative(final String path) {
-		
+
 		return this.relative(path, null);
 	}
-	
+
 	/** - empty path returns not 'this' but 'null';
 	 *
 	 * - paths starting with '/' are still relative to 'this' entry.
@@ -414,10 +414,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Entry relative(final String path, final TreeLinkType mode) {
-		
-		return this.relative(this, path, mode);
+
+		return Entry.relative(this, this, path, mode);
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -426,10 +426,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Value<? extends TransferCopier> relativeBinary(final ExecProcess ctx, final String path) {
-		
+
 		return this.relativeFile(path).getBinaryContent();
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -438,74 +438,74 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionHidden
 	default TransferCopier relativeBinary(final String path) {
-		
+
 		return this.relativeFile(path).toBinary().getBinary();
 	}
-	
+
 	/** @param path
 	 * @return */
 	@ReflectionExplicit
 	default Entry relativeFile(final String path) {
-		
+
 		return this.relative(path, TreeLinkType.PUBLIC_TREE_REFERENCE);
 	}
-	
+
 	/** Cannot traverse above 'this'.
 	 *
 	 * @param path
 	 * @return */
 	@ReflectionHidden
 	default Entry relativeFileEnsure(final Path path) {
-		
+
 		return null;
 	}
-	
+
 	/** Cannot traverse above 'this'.
 	 *
 	 * @param path
 	 * @return */
 	@ReflectionExplicit
 	default Entry relativeFileEnsure(final String path) {
-		
+
 		final Entry result = this.relative(path, TreeLinkType.PUBLIC_TREE_REFERENCE);
 		if (!result.isExist() && result.doSetBinary(TransferCopier.NUL_COPIER).baseValue() != Boolean.TRUE) {
 			throw new RuntimeException("Can't create an empty file: " + result);
 		}
 		return result;
 	}
-	
+
 	/** @param path
 	 * @return */
 	@ReflectionExplicit
 	default EntryContainer relativeFolder(final String path) {
-		
+
 		return this.relative(path, TreeLinkType.PUBLIC_TREE_REFERENCE).toContainer();
 	}
-	
+
 	/** Cannot traverse above 'this'.
 	 *
 	 * @param path
 	 * @return */
 	@ReflectionHidden
 	default EntryContainer relativeFolderEnsure(final Path path) {
-		
+
 		return null;
 	}
-	
+
 	/** Cannot traverse above 'this'.
 	 *
 	 * @param path
 	 * @return */
 	@ReflectionExplicit
 	default EntryContainer relativeFolderEnsure(final String path) {
-		
+
 		final EntryContainer result = this.relative(path, TreeLinkType.PUBLIC_TREE_REFERENCE).toContainer();
 		if (!result.isExist() && result.doSetContainer().baseValue() != Boolean.TRUE) {
 			throw new RuntimeException("Can't create a folder: " + result);
 		}
 		return result;
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -514,18 +514,18 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Object relativePrimitive(final ExecProcess ctx, final String path) {
-		
+
 		return this.relativeFile(path).toPrimitive().getPrimitiveValue(ctx);
 	}
-	
+
 	/** @param path
 	 * @return */
 	@ReflectionHidden
 	default Object relativePrimitive(final String path) {
-		
+
 		return this.relativeFile(path).toPrimitive().getPrimitiveValue();
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -534,10 +534,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Value<? extends CharSequence> relativeText(final ExecProcess ctx, final String path) {
-		
+
 		return this.relativeFile(path).getTextContent();
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -546,10 +546,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionHidden
 	default CharSequence relativeText(final String path) {
-		
+
 		return this.relativeFile(path).toCharacter().getText();
 	}
-	
+
 	/** Works when isBinary() method returns TRUE.
 	 *
 	 * Returns binary entry instance.
@@ -561,10 +561,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default EntryBinary toBinary() {
-		
+
 		return null;
 	}
-	
+
 	/** Works when isText() method returns TRUE.
 	 *
 	 * Returns a text entry instance.
@@ -576,10 +576,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default EntryCharacter toCharacter() {
-		
+
 		return null;
 	}
-	
+
 	/** Works when isContainer() method returns TRUE.
 	 *
 	 * Returns container entry instance.
@@ -591,10 +591,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default EntryContainer toContainer() {
-		
+
 		return null;
 	}
-	
+
 	/** Works when isMount() method returns TRUE.
 	 *
 	 * Returns mount entry instance.
@@ -606,10 +606,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default EntryMount toMount() {
-		
+
 		return null;
 	}
-	
+
 	/** Works when isPrimitive() method returns TRUE.
 	 *
 	 * Returns primitive entry instance.
@@ -621,10 +621,10 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default EntryPrimitive toPrimitive() {
-		
+
 		return null;
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -634,7 +634,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Object updateRelativeDate(final ExecProcess ctx, final String path, final Object primitive) {
-		
+
 		final Entry e = this.relativeFile(path);
 		if (e.isPrimitive() && BaseDate.equalsGeneric(primitive, e.toPrimitive().getPrimitiveValue(ctx))) {
 			return primitive;
@@ -642,7 +642,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 		e.doSetDate(primitive);
 		return primitive;
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -652,7 +652,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Object updateRelativePrimitive(final ExecProcess ctx, final String path, final BaseObject primitive) {
-		
+
 		final Entry e = this.relativeFile(path);
 		if (e.isPrimitive() && primitive == e.toPrimitive().getPrimitiveValue(ctx)) {
 			return primitive;
@@ -660,7 +660,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 		e.doSetPrimitive(primitive);
 		return primitive;
 	}
-	
+
 	/** TODO: add default value and make use of java/async
 	 *
 	 * @param ctx
@@ -670,7 +670,7 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 	 * @return */
 	@ReflectionExplicit
 	default Object updateRelativeText(final String path, final CharSequence text) {
-		
+
 		final Entry e = this.relativeFile(path);
 		if (e.isPrimitive() && text.equals(e.toCharacter().getText())) {
 			return text;
@@ -678,5 +678,5 @@ public interface Entry extends EntryChange /* FIXME: temporarily, decouple later
 		e.doSetText(text);
 		return text;
 	}
-	
+
 }
--- java/ru/myx/ae3/vfs/VfsInterface.java
+++ java/ru/myx/ae3/vfs/VfsInterface.java
@@ -3,81 +3,44 @@ package ru.myx.ae3.vfs;
 import ru.myx.ae3.base.BaseObject;
 import ru.myx.ae3.know.Guid;
 
-/**
- * 
- * @author myx
- * 
- */
+/** @author myx */
 public interface VfsInterface {
-	/**
-	 * 
-	 * @return
-	 */
+
+	/** @return */
 	TransactionVfs createTransaction();
-	
-	
-	/**
-	 * 
-	 * @param entry
-	 */
-	void doUnlink(
-			Entry entry);
-	
-	
-	/**
-	 * 
-	 * @param container
-	 * @param key
-	 */
-	void doUnlink(
-			Entry container,
-			BaseObject key);
-	
-	
-	/**
-	 * 
-	 * @param container
-	 * @param key
-	 */
-	void doUnlink(
-			Entry container,
-			Guid key);
-	
-	
-	/**
-	 * 
-	 * @param container
-	 * @param key
-	 */
-	void doUnlink(
-			Entry container,
-			String key);
-	
-	
-	/**
-	 * 
-	 * @param share
+
+	/** @param entry */
+	void doUnlink(Entry entry);
+
+	/** @param container
+	 * @param key */
+	void doUnlink(Entry container, BaseObject key);
+
+	/** @param container
+	 * @param key */
+	void doUnlink(Entry container, Guid key);
+
+	/** @param container
+	 * @param key */
+	void doUnlink(Entry container, String key);
+
+	/** @param share
 	 * @param focus
 	 * @param path
 	 * @param mode
-	 * @return
-	 */
-	Entry getRelative(
-			Entry share,
-			Entry focus,
-			String path,
-			TreeLinkType mode);
-	
-	
-	/**
-	 * 
-	 * @param root
+	 * @return */
+	default Entry getRelative(final Entry share, final Entry focus, final String path, final TreeLinkType mode) {
+
+		return Entry.relative(share, focus, path, mode);
+	}
+
+	/** @param root
 	 * @param path
 	 * @param mode
-	 * @return
-	 */
-	Entry getRelative(
-			Entry root,
-			String path,
-			TreeLinkType mode);
+	 * @return */
+	default Entry getRelative(final Entry root, final String path, final TreeLinkType mode) {
+
+		return Entry.relative(root, root, path, mode);
+
+	}
 }
