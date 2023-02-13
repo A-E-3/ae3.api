da71f69689f6e5c606db925ffa678245bfdea488
Alexander Kharichev <myx@myx.ru>
2023-02-13 21:09:54 +0200

    * vfs: less fails on unlink

--- java/ru/myx/ae3/vfs/ars/ArsReference.java
+++ java/ru/myx/ae3/vfs/ars/ArsReference.java
@@ -13,22 +13,44 @@ public interface ArsReference<O extends ArsRecord> {
 
 	/** @return */
 	O getKey();
-	
+
 	/** @return */
 	String getKeyString();
-	
+
 	/** @return */
 	long getLastModified();
-	
+
 	/** @return */
 	TreeLinkType getLinkageMode();
-	
+
 	/** @return */
 	O getSource();
-	
+
 	/** @return */
 	O getTarget();
-	
+
 	/** @return */
 	boolean isExist();
+
+	/** Checks that it is container or wildcard template of throws exception
+	 *
+	 * TODO: use to mark template references
+	 *
+	 * @return
+	 * @throws IllegalStateException */
+	default ArsReference<O> toBinaryReference() throws IllegalStateException {
+
+		return this;
+	}
+
+	/** Checks that it is container or wildcard template of throws exception
+	 *
+	 * TODO: use to mark template references
+	 *
+	 * @return
+	 * @throws IllegalStateException */
+	default ArsReference<O> toContainerReference() throws IllegalStateException {
+
+		return this;
+	}
 }
