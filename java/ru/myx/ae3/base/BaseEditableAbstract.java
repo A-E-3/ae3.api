94bf8ab17f920cd814c9efd5c67824a3c144ad8c
Alex Kharichev <myx@myx.nz>
2025-03-28 16:21:54 +0200

    * BaseEditableAbstract - refactor setOwn methods

--- java/ru/myx/ae3/base/BaseEditableAbstract.java
+++ java/ru/myx/ae3/base/BaseEditableAbstract.java
@@ -7,15 +7,15 @@ import ru.myx.ae3.reflect.ReflectionDisable;
 /** @author myx */
 @ReflectionDisable
 public abstract class BaseEditableAbstract extends BaseAbstract implements BaseEditable {
-	
+
 	/**
 	 *
 	 */
 	protected BaseProperties<?> properties = null;
-	
+
 	@Override
 	public boolean baseDefine(final BasePrimitiveString name, final BaseObject value, final short attributes) {
-		
+
 		assert name != null : "Name is NULL";
 		assert value != null : "Shouldn't be NULL, use BaseObject.UNDEFINED or BaseObject.NULL instead";
 		/** 1. Call the [[GetOwnProperty]] method of O with property name P.<br>
@@ -66,10 +66,10 @@ public abstract class BaseEditableAbstract extends BaseAbstract implements BaseE
 		 */
 		return property.propertySet(this, name, value, attributes);
 	}
-	
+
 	@Override
 	public boolean baseDefine(final String name, final BaseObject value, final short attributes) {
-		
+
 		assert name != null : "Name is NULL";
 		assert value != null : "Shouldn't be NULL, use BaseObject.UNDEFINED or BaseObject.NULL instead";
 		/** 1. Call the [[GetOwnProperty]] method of O with property name P.<br>
@@ -120,7 +120,7 @@ public abstract class BaseEditableAbstract extends BaseAbstract implements BaseE
 		 */
 		return property.propertySet(this, name, value, attributes);
 	}
-	
+
 	/** [[Delete]] (P) When the [[Delete]] method of O is called with property name P, the following
 	 * steps are taken:<br>
 	 * 1. If O doesn’t have a property with name P, return true.<br>
@@ -131,9 +131,9 @@ public abstract class BaseEditableAbstract extends BaseAbstract implements BaseE
 	 * @param name
 	 * @return boolean */
 	public boolean baseDelete(final BasePrimitiveString name) {
-		
+
 		assert name != null : "property name shouldn't be NULL";
-		
+
 		final BaseProperties<?> properties = this.properties;
 		if (properties == null) {
 			return true;
@@ -149,7 +149,7 @@ public abstract class BaseEditableAbstract extends BaseAbstract implements BaseE
 		}
 		return false;
 	}
-	
+
 	/** [[Delete]] (P) When the [[Delete]] method of O is called with property name P, the following
 	 * steps are taken:<br>
 	 * 1. If O doesn’t have a property with name P, return true.<br>
@@ -161,7 +161,7 @@ public abstract class BaseEditableAbstract extends BaseAbstract implements BaseE
 	 * @return boolean */
 	@Override
 	public boolean baseDelete(final String name) {
-		
+
 		assert name != null : "property name shouldn't be NULL";
 		final BaseProperties<?> properties = this.properties;
 		if (properties == null) {
@@ -178,151 +178,126 @@ public abstract class BaseEditableAbstract extends BaseAbstract implements BaseE
 		}
 		return false;
 	}
-	
+
 	@Override
 	public BaseProperty baseGetOwnProperty(final BasePrimitiveString name) {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties == null
 			? null
 			: properties.find(name);
 	}
-	
+
 	@Override
 	public BaseProperty baseGetOwnProperty(final CharSequence name) {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties == null
 			? null
 			: properties.find(name);
 	}
-	
+
 	@Override
 	public BaseProperty baseGetOwnProperty(final String name) {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties == null
 			? null
 			: properties.find(name);
 	}
-	
+
 	@Override
 	public boolean baseHasKeysOwn() {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties != null && properties.hasEnumerableProperties();
 	}
-	
+
 	@Override
 	public Iterator<String> baseKeysOwn() {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties == null
 			? BaseObject.ITERATOR_EMPTY
 			: properties.iteratorEnumerableAsString();
 	}
-	
+
 	@Override
 	public Iterator<? extends CharSequence> baseKeysOwnAll() {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties == null
 			? BaseObject.ITERATOR_EMPTY
 			: properties.iteratorAllAsString();
 	}
-	
+
 	@Override
 	public Iterator<? extends BasePrimitive<?>> baseKeysOwnPrimitive() {
-		
+
 		final BaseProperties<?> properties = this.properties;
 		return properties == null
 			? BaseObject.ITERATOR_EMPTY_PRIMITIVE
 			: properties.iteratorEnumerableAsPrimitive();
 	}
-	
+
 	// @Override
 	// public abstract boolean equals(final Object o);
-	
+
 	// @Override
 	// public abstract int hashCode();
-	
+
 	@Override
-	public boolean setOwnProperty(final BasePrimitiveString name, final BaseProperty property, final short attributes) {
-		
+	public final boolean setOwnProperty(final BasePrimitiveString name, final BaseProperty property, final short attributes) {
+
 		assert property != null : "NULL property";
-		
+
 		final BaseProperties<?> properties = this.properties;
-		
-		if (properties != null) {
-			final BaseProperty existing = properties.find(name);
-			if (existing != null) {
-				final short existingAttributes = existing.propertyAttributes(name);
-				if ((existingAttributes & BaseProperty.ATTRS_MASK_WND) == 0) {
-					// ignore
-					return false;
-				}
-				
-				// assert AbstractSAPI.logDebug("ROPI(this=" +
-				// this.getClass().getSimpleName() + ", key=" + name + ",
-				// propClass=" + property.getClass().getSimpleName() + ")");
-				
-				final BaseProperties<?> replacement = properties.delete(name);
-				
-				this.properties = replacement == null
-					? BaseProperties.createFirstProperty(this, name, property, attributes)
-					: replacement.add(this, name, property, attributes);
-				return true;
-			}
+		if (properties == null) {
+			this.properties = BaseProperties.createFirstProperty(this, name, property, attributes);
+			return true;
 		}
-		
-		// assert AbstractSAPI.logDebug("SOPI(this=" +
-		// this.getClass().getSimpleName() + ", key=" + name + ", propClass=" +
-		// property.getClass().getSimpleName() + ")");
-		
-		/** including BasePropertyFilter */
-		this.properties = properties == null
-			? BaseProperties.createFirstProperty(this, name, property, attributes)
-			: properties.add(this, name, property, attributes);
+
+		final BaseProperty existing = properties.find(name);
+		if (existing == null) {
+			this.properties = properties.add(this, name, property, attributes);
+			return true;
+		}
+
+		if ((existing.propertyAttributes(name) & BaseProperty.ATTRS_MASK_WND) == 0) {
+			// ignore
+			return false;
+		}
+
+		/** will gracefully replace the existing one **/
+		this.properties = properties.add(this, name, property, attributes);
 		return true;
 	}
-	
+
 	@Override
-	public boolean setOwnProperty(final String name, final BaseProperty property, final short attributes) {
-		
+	public final boolean setOwnProperty(final String name, final BaseProperty property, final short attributes) {
+
 		assert property != null : "NULL property";
-		
+
 		final BaseProperties<?> properties = this.properties;
-		
-		if (properties != null) {
-			final BaseProperty existing = properties.find(name);
-			if (existing != null) {
-				if ((existing.propertyAttributes(name) & BaseProperty.ATTRS_MASK_WND) == 0) {
-					// ignore
-					return false;
-				}
-				
-				// assert AbstractSAPI.logDebug("ROPS(this=" +
-				// this.getClass().getSimpleName() + ", key=" + name + ",
-				// propClass=" + property.getClass().getSimpleName() + ")");
-				
-				final BaseProperties<?> replacement = properties.delete(name);
-				
-				this.properties = replacement == null
-					? BaseProperties.createFirstProperty(this, name, property, attributes)
-					: replacement.add(this, name, property, attributes);
-				return true;
-			}
+		if (properties == null) {
+			this.properties = BaseProperties.createFirstProperty(this, name, property, attributes);
+			return true;
+		}
+
+		final BaseProperty existing = properties.find(name);
+		if (existing == null) {
+			this.properties = properties.add(this, name, property, attributes);
+			return true;
 		}
 		
-		// assert AbstractSAPI.logDebug("SOPS(this=" +
-		// this.getClass().getSimpleName() + ", key=" + name + ", propClass=" +
-		// property.getClass().getSimpleName() + ")");
+		if ((existing.propertyAttributes(name) & BaseProperty.ATTRS_MASK_WND) == 0) {
+			// ignore
+			return false;
+		}
 		
-		/** including BasePropertyFilter */
-		this.properties = properties == null
-			? BaseProperties.createFirstProperty(this, name, property, attributes)
-			: properties.add(this, name, property, attributes);
+		/** will gracefully replace the existing one **/
+		this.properties = properties.add(this, name, property, attributes);
 		return true;
 	}
-	
+
 }
