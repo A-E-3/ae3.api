95f07ecf61a2afa0138209598141f8905b76f187
Alex Kharichev <myx@myx.nz>
2025-04-01 13:55:26 +0300

    * minor: source formatting whitespace

--- java/ru/myx/ae3/base/BaseError.java
+++ java/ru/myx/ae3/base/BaseError.java
@@ -4,83 +4,68 @@ import ru.myx.ae3.reflect.Reflect;
 import ru.myx.ae3.reflect.ReflectionExplicit;
 import ru.myx.ae3.reflect.ReflectionManual;
 
-/**
- * Base Error Fixed Properties
- * 
- * 
- * @author myx
- * 		
- */
+/** Base Error Fixed Properties
+ *
+ *
+ * @author myx */
 @ReflectionManual
 public interface BaseError extends BaseObjectNotDynamic {
-	
+
 	/**
-	 * 
+	 *
 	 */
 	BaseObject PROTOTYPE = Reflect.classToBasePrototype(BaseError.class);
-	
-	/**
-	 * The value of the [[Class]] property is defined by this specification for
-	 * every kind of built-in object. The value of the [[Class]] property of a
-	 * host object may be any value, even a value used by a built-in object for
-	 * its [[Class]] property. The value of a [[Class]] property is used
-	 * internally to distinguish different kinds of built-in objects. Note that
-	 * this specification does not provide any means for a program to access
-	 * that value except through Object.prototype.toString (see 15.2.4.2).
-	 * 
-	 * @return class
-	 */
+
+	/** The value of the [[Class]] property is defined by this specification for every kind of
+	 * built-in object. The value of the [[Class]] property of a host object may be any value, even
+	 * a value used by a built-in object for its [[Class]] property. The value of a [[Class]]
+	 * property is used internally to distinguish different kinds of built-in objects. Note that
+	 * this specification does not provide any means for a program to access that value except
+	 * through Object.prototype.toString (see 15.2.4.2).
+	 *
+	 * @return class */
 	@Override
 	default String baseClass() {
-		
+
 		return "Error";
 	}
-	
-	@Override
-	default BaseObject basePrototype() {
-		
-		return BaseError.PROTOTYPE;
-	}
-	
+
 	@Override
 	default BaseProperty baseGetOwnProperty(final BasePrimitiveString name) {
-		
+
 		return null;
 	}
-	
+
 	@Override
 	default BaseProperty baseGetOwnProperty(final String name) {
-		
+
 		return null;
 	}
-	
+
+	@Override
+	default BaseObject basePrototype() {
+
+		return BaseError.PROTOTYPE;
+	}
+
 	@Override
 	default Object baseValue() {
-		
+
 		return this;
 	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	@ReflectionExplicit
-			String getMessage();
-			
-	/**
-	 * 
-	 * @return
-	 */
-	@ReflectionExplicit
-			Object getMessageContent();
-			
-	/**
-	 * 
-	 * @return
-	 */
+
+	/** @return */
 	@ReflectionExplicit
 	default Throwable getCause() {
-		
+
 		return null;
 	}
+
+	/** @return */
+	@ReflectionExplicit
+	String getMessage();
+
+	/** @return */
+	@ReflectionExplicit
+	Object getMessageContent();
 }
