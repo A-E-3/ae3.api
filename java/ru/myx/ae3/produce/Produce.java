8c10fa165b0fe0c008044a66f533b6796a7b04e7
Alex Kharichev <myx@myx.nz>
2025-05-09 00:20:16 +0300

    ! Produce.connectLeast must check chains accepted types

--- java/ru/myx/ae3/produce/Produce.java
+++ java/ru/myx/ae3/produce/Produce.java
@@ -20,22 +20,22 @@ import ru.myx.ae3.report.Report;
  *         myx - barachta "typecomment": Window>Preferences>Java>Templates. To enable and disable
  *         the creation of type comments go to Window>Preferences>Java>Code Generation. */
 public class Produce extends AbstractSAPI {
-	
+
 	private static final FactoryRegistry factoryRegistry;
-	
+
 	private static final AbstractProduceImpl PRODUCE_IMPL;
-	
+
 	static {
 		PRODUCE_IMPL = AbstractSAPI.createObject("ru.myx.ae3.produce.ImplementProduce");
 		factoryRegistry = new FactoryRegistry();
 		Produce.PRODUCE_IMPL.registerDefaultFactories();
 	}
-	
+
 	private static final void warning(final String message) {
-		
+
 		Report.currentReceiverLog().event("AE3.PRODUCE", "WARNING", message);
 	}
-	
+
 	/** Creates an object flow connection using all available factories registered in the system.
 	 * Any factories can produce objects via this method.
 	 *
@@ -54,11 +54,11 @@ public class Produce extends AbstractSAPI {
 			final Class<?> context,
 			final ObjectTarget<?> chain //
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (ObjectTarget<T>) Produce.factoryRegistry.targetConnected(target, type, attributes, context, chain);
 	}
-	
+
 	/** @param <T>
 	 * @param name
 	 * @param parent
@@ -74,24 +74,24 @@ public class Produce extends AbstractSAPI {
 			final String sourceFactory,
 			final BaseObject sourceAttributes//
 	) throws Exception {
-		
+
 		if (parent == null) {
 			throw new IllegalArgumentException("Parent is NULL: name=" + name);
 		}
-		
+
 		final ObjectTarget<T> target = Produce.targetLeast(name, parent, chains, 0);
 		if (target == null) {
 			return false;
 		}
-		
-		final ObjectSource<T> source = Produce.source(parent.accepts(), sourceFactory, sourceAttributes, null);
+
+		final ObjectSource<T> source = Produce.source(target.accepts(), sourceFactory, sourceAttributes, null);
 		if (source == null) {
 			return false;
 		}
-
+		
 		return Flow.connect(source, target);
 	}
-
+	
 	/** Retrieves a factory to produce objects of specified type.
 	 *
 	 * @param target
@@ -108,11 +108,11 @@ public class Produce extends AbstractSAPI {
 			final BaseObject attributes,
 			final Class<? extends S> context//
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (ObjectFactory<S, T>) Produce.factoryRegistry.factory(target, type, attributes, context);
 	}
-	
+
 	/** Retrieves a factory to produce objects of specified type.
 	 *
 	 * @param target
@@ -129,11 +129,11 @@ public class Produce extends AbstractSAPI {
 			final Map<String, Object> attributes,
 			final Class<? extends S> context//
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (ObjectFactory<S, T>) Produce.factoryRegistry.factory(target, type, Base.fromMap(attributes), context);
 	}
-	
+
 	/** Creates an object using all available factories registered in the system. Only blocking
 	 * factories can produce objects via this method.
 	 *
@@ -150,17 +150,17 @@ public class Produce extends AbstractSAPI {
 			final BaseObject attributes,
 			final Object context//
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (T) Produce.factoryRegistry.create(target, type, attributes, context);
 	}
-	
+
 	/** @param factory */
 	public static final void registerFactory(final ObjectFactory<?, ?> factory) {
-		
+
 		Produce.factoryRegistry.register(factory);
 	}
-	
+
 	/** Creates an object using all available factories registered in the system. This is the only
 	 * way for non-blocking factory to produce objects.
 	 *
@@ -177,11 +177,11 @@ public class Produce extends AbstractSAPI {
 			final BaseObject attributes,
 			final Object context//
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (ObjectSource<T>) Produce.factoryRegistry.sourcePrepared(target, type, attributes, context);
 	}
-	
+
 	/** Creates an object using all available factories registered in the system. This is the only
 	 * way for non-blocking factory to produce objects.
 	 *
@@ -198,11 +198,11 @@ public class Produce extends AbstractSAPI {
 			final Map<String, Object> attributes,
 			final Object context//
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (ObjectSource<T>) Produce.factoryRegistry.sourcePrepared(target, type, Base.fromMap(attributes), context);
 	}
-	
+
 	/** Creates an object target using all available factories registered in the system. Any
 	 * factories can produce objects via this method.
 	 *
@@ -221,11 +221,11 @@ public class Produce extends AbstractSAPI {
 			final Class<?> context,
 			final ObjectTarget<?> chain //
 	) {
-		
+
 		assert target != null : "target class agrument should not be NULL!";
 		return (ObjectTarget<T>) Produce.factoryRegistry.targetConnected(target, type, attributes, context, chain);
 	}
-
+	
 	/** @param <T>
 	 * @param name
 	 * @param parent
@@ -238,9 +238,8 @@ public class Produce extends AbstractSAPI {
 			final String name,
 			final ObjectTarget<T> parent,
 			final BaseObject[] chains,
-			final int chainIndex
-	) throws Exception {
-		
+			final int chainIndex) throws Exception {
+
 		if (parent == null) {
 			throw new IllegalArgumentException("Parent is NULL: name=" + name);
 		}
@@ -280,10 +279,10 @@ public class Produce extends AbstractSAPI {
 		Produce.warning("[" + name + "] chain multi failed!");
 		return Produce.targetLeast(name, parent, chains, chainIndex + 1);
 	}
-	
+
 	private Produce() {
-		
+
 		// empty
 	}
-	
+
 }
